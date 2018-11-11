1d38edf90ff29a0f1ea54a960f125bd288688eeb
Shivam Pandey <pshivam97@gmail.com>
2018-11-11 13:25:55 +0530

    Updated

---
+++ Our Work/src/word_recognition.py
@@ -0,0 +1,28 @@
+from image_preprocess import split_sentence, disp_image
+import tensorflow as tf
+import os
+from shutil import copyfile
+import subprocess
+
+def get_recognized_words_list(sentence_path) :
+    recognized_words_list = list()
+    words_image_list = split_sentence(sentence_path)
+
+    for each_word_image in words_image_list :
+        copyfile(each_word_image, "../data/test.png")
+        result = subprocess.run(['python3', 'main.py'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        result = result.strip()
+        recognized_word = str()
+
+        for each_char_index in range(len(result)-2 , 0 , -1) :
+            if result[each_char_index] == '"' :
+                recognized_word = recognized_word[::-1]
+                recognized_words_list.append(recognized_word)
+                break
+            else :
+                recognized_word += result[each_char_index]
+
+    os.system("rm w*.png")
+    return recognized_words_list
+
+print(get_recognized_words_list('test7.png'))
